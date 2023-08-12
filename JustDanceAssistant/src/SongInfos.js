57d099d64799a378269c75fe8c74d639f3aeadcb
Unico417 <0417unico@gmail.com>
2023-08-13 02:54:13 +0900

    JustDanceAssistant - 楽曲情報追加

--- JustDanceAssistant/src/SongInfos.js
+++ JustDanceAssistant/src/SongInfos.js
@@ -3,290 +3,307 @@ const SONGINFO = [
         artist: "Soulja Boy Tell 'Em",
         title: 'Crank That',
         url: 'https://youtu.be/MqqKTpXNiRw',
-        level: 3
-    },
-    {
+        level: 4
+    }, {
         artist: 'K/DA',
         title: 'POP☆STARS',
         url: 'https://youtu.be/tL8u77b5w8I',
         level: 9
-    },
-    {
+    }, {
         artist: 'Ke$ha',
         title: 'TiK ToK',
         url: 'https://youtu.be/xUPSMPo1l4U',
         level: 1
-    },
-    {
+    }, {
         artist: 'The Chainsmokers',
         title: 'Closer',
         url: 'https://youtu.be/3ehiqCmP8Iw',
         level: 1
-    },
-    {
+    }, {
         artist: 'Skrillex',
         title: 'Rock n Roll',
         url: 'https://youtu.be/IDn3Kimu4DU',
         level: 2
-    },
-    {
+    }, {
         artist: 'Ylvis',
         title: 'What Does The Fox Say?',
         url: 'https://youtu.be/9dzFkU9VpJQ',
         level: 1
-    },
-    {
+    }, {
         no: 2135,
         artist: 'will.i.am',
         title: '#thatPOWER feat. Justin Bieber',
         inGameTitle: '#thatPOWER [Extreme]',
         category: 'JustDance Solo',
         level: 4,
-    },
-    {
+    }, {
         artist: 'will.i.am',
         title: '#thatPOWER feat. Justin Bieber',
         inGameTitle: '#thatPOWER [On-Stage]',
         category: 'JustDance Trio',
-    },
-    {
+    }, {
         artist: 'will.i.am',
         title: '#thatPOWER feat. Justin Bieber',
         inGameTitle: '#thatPOWER',
         category: 'JustDance Crew',
-    },
-    {
+        level: 3,
+    }, {
         no: 2203,
         artist: 'Queen',
         title: 'Another One Bites the Dust',
         inGameTitle: 'Another One Bites the Dust [Stunt]',
         category: 'JustDance Solo',
         level: 3,
-    },
-    {
+    }, {
         no: 2241,
         artist: 'Calvin Harris',
         title: 'Blame feat. John Newman',
         inGameTitle: 'Blame',
         category: 'JustDance Solo',
         level: 3,
-    },
-    {
+    }, {
         no: 2275,
         artist: 'Carly Rae Jepsen',
         title: 'Call Me Maybe',
         inGameTitle: 'Call Me Maybe [Alternate]',
         category: 'JustDance Solo',
         level: 3,
-    },
-    {
+    }, {
         no: 2289,
         artist: 'Macklemore & Ryan Lewis',
         title: "Can't Hold Us",
         inGameTitle: "Can't Hold Us",
         category: 'JustDance Solo',
         level: 3
-    },
-    {
+    }, {
         no: 2295,
         artist: 'Britney Spears',
         title: 'Circus',
         inGameTitle: 'Circus [Extreme]',
         category: 'JustDance Solo',
         level: 5,
-    },
-    {
+    }, {
         no: 2323,
         artist: 'Luis Fonsi',
         title: 'Despacito feat. Daddy Yankee',
         inGameTitle: 'Despacito [Extreme]',
         category: 'JustDance Solo',
         level: 5,
-    },
-    {
+    }, {
         no: 2334,
         artist: 'Queen',
         title: "Don't Stop Me Now",
         inGameTitle: "Don't Stop Me Now",
         category: 'JustDance Solo',
         level: 5,
-    },
-    {
+    }, {
         artist: 'Queen',
         title: "Don't Stop Me Now",
         inGameTitle: "Don't Stop Me Now [Panda]",
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Kenny Loggins',
         title: 'Footloose',
         inGameTitle: 'Footloose',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Flo Rida',
         title: 'Good Feeling',
         inGameTitle: 'Good Feeling [Extreme]',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Pharrell Williams',
         title: 'Happy',
         inGameTitle: 'Happy',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Bog Bog Orkestar',
         title: 'Isidora',
         inGameTitle: 'Isidora (HARD)',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Toño Rosario',
         title: 'Kulikitaka',
         inGameTitle: '[2021] Kulikitaka (nekodance?)',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Wilson Pickett',
         title: 'Land of 1000 Dances',
         inGameTitle: 'Land Of 1000 Dances',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         artist: 'Maroon 5',
         title: 'Moves Like Jagger feat. Christina Aguilera',
         inGameTitle: 'Moves Like Jagger',
         category: 'JustDance Solo',
-    },
-    {
+    }, {
         no: 2591,
         artist: 'LMFAO',
         title: 'Party Rock Anthem',
         inGameTitle: 'Party Rock Anthem',
         category: 'JustDance Solo',
         level: 1,
-    },
-    {
+    }, {
         artist: 'Boney M.',
         title: 'Rasputin',
         inGameTitle: 'Rasputin',
         category: 'JustDance Solo',
         level: 5
-    },
-    {
+    }, {
         no: 2189,
         artist: 'DJ OZMA',
         title: 'アゲ♂アゲ♂EVERY☆騎士',
         inGameTitle: 'Age Age Every Knight (アゲ♂アゲ♂EVERY☆騎士)',
         category: 'JustDance Solo',
         level: 2
-    },
-    {
+    }, {
         no: 2315,
         artist: 'TONES AND I',
         title: 'Dance Monkey',
         inGameTitle: 'Dance Monkey',
         category: 'JustDance Solo',
         level: 1
-    },
-    {
+    }, {
         no: 476,
         artist: 'Zedd & Jasmine Thompson',
         title: 'Funny',
         inGameTitle: 'Zedd & Jasmine Thompson - Funny',
         category: 'Fitness Marshall',
         level: 3
-    },
-    {
+    }, {
         no: 2383,
         artist: 'PSY',
         title: 'Gentleman',
         inGameTitle: 'Gentleman',
         category: 'JustDance Solo',
         level: 1
-    },
-    {
+    }, {
         no: 2540,
         artist: 'Dee Dee Sharp',
         title: 'Mashed Potato Time',
         inGameTitle: 'Mashed Potato Time',
         category: 'JustDance Solo',
         level: 1
-    },
-    {
+    }, {
         no: 2565,
         artist: 'VAVA',
         title: 'My New Swag (我的新衣) feat Ty. & 王倩倩',
         inGameTitle: 'My New Swag (我的新衣)',
         category: 'JustDance Solo',
-        level: 2
-    },
-    {
+        level: 3
+    }, {
         no: 2697,
         artist: 'Live Tune feat. 初音ミク',
         title: 'Tell Your World',
         inGameTitle: 'Tell Your World',
         category: 'JustDance Solo',
         level: 3
-    },
-    {
+    }, {
         no: 2753,
         artist: 'Spicy Girl',
         title: 'Wannabe',
         inGameTitle: 'Wannabe',
         category: 'JustDance Solo',
         level: 2
-    },
-    {
+    }, {
         no: 3063,
         artist: 'ABBA',
         title: 'Gimme! Gimme! Gimme!',
         inGameTitle: 'Gimme! Gimme! Gimme! (A Men After Midnight)',
         category: 'JustDance Solo',
         level: 2
-    },
-    {
+    }, {
         no: 248,
         artist: 'The Chainsmoker',
         title: 'Closer',
         inGameTitle: 'Closer',
         category: 'Fitness Marshall',
         level: 4
-    },
-    {
+    }, {
         no: 2780,
         artist: 'Fifth Harmony',
         title: 'Worth It feat. Kid Ink',
         inGameTitle: 'Worth It',
         category: 'JustDance Solo',
         level: 3
-    },
-    {
+    }, {
         no: 2728,
         artist: 'Olly Murs',
         title: 'Troublemaker feat. Flo Rida',
         inGameTitle: 'Troublemaker [Sweet]',
         category: 'JustDance Solo',
         level: 2
-    },
-    {
+    }, {
         no: 218,
         artist: 'Sawtee',
         title: 'Best Friend feat. Doja Cat',
         inGameTitle: 'Best Friend - Sawtee feat. Doja Cat',
         category: 'Fitness Marshall',
         level: 4
-    },
-    {
+    }, {
         no: 232,
         artist: 'Ariana Grande',
         title: '34+35 feat. Doja Cat, Megan Thee Stallion',
         inGameTitle: 'Ariana Grande - 34+35 feat. Doja Cat, Megan Thee Stallion',
         category: 'Fitness Marshall',
         level: 2
+    }, {
+        no: 225,
+        artist: 'Doja Cat',
+        title: 'Boss B*tch',
+        inGameTitle: 'Boss B*tch',
+        category: 'Fitness Marshall',
+        level: 3
+    }, {
+        no: 266,
+        artist: 'Dua Lipa',
+        title: 'Break My Heart',
+        inGameTitle: 'Break My Heart',
+        category: 'Fitness Marshall',
+        level: 3
+    }, {
+        no: 2272,
+        artist: 'Katy Perry',
+        title: 'California Gurls',
+        inGameTitle: 'California Gurls',
+        category: 'JustDance Solo',
+        level: 1
+    }, {
+        no: 3169,
+        artist: 'NCT 127',
+        title: 'Kick It',
+        inGameTitle: 'Kick It',
+        category: 'JustDance Solo',
+        level: 3
+    }, {
+        no: 2812,
+        artist: 'Martin Garrix',
+        title: 'Animals',
+        inGameTitle: 'Animals',
+        category: 'JustDance Solo',
+        level: 3
+    }, {
+        no: 2964,
+        artist: 'Doja Cat',
+        title: 'Say So',
+        inGameTitle: 'Say So',
+        category: 'JustDance Solo',
+        level: 2
+    }, {
+        no: 2968,
+        artist: 'Little Big',
+        title: 'Skibidi',
+        inGameTitle: 'Skibidi',
+        category: 'JustDance Solo',
+        level: 1
+    }, {
+        no: 181,
+        artist: 'Sam Smith, Kim Petras',
+        title: 'Unholy',
+        inGameTitle: 'Unholy - @samsmith & Kim Petras',
+        category: 'Fitness Marshall',
+        level: 3
     },
-
 ];
 
 export { SONGINFO };
